6ce025857b83ddec7f5cf80b7e545be0258d1c61
mkholilb-max <mkholil6b@gmail.com>
2025-11-01 15:17:50 +0700

    addjs

---
+++ data/index.js
@@ -0,0 +1,54 @@
+
+
+export const textImg = [{
+    id:1,
+    img: "assets/icon2.png",
+    title:"1.Start",
+    paragraf:"Pilih salah satu game yang ingin kamu top up"
+
+},{
+    id:2,
+    img: "assets/icon1.png",
+    title:"2. Fill Up",
+    paragraf:'Top up sesuai dengan nominal yang sudah tersedia'
+},{
+    id:3,
+    img: "assets/icon.png",
+    title:"3. Be a winner",
+    paragraf:'Siap digunakan untuk improve permainan kamu'   
+}] 
+
+export const ImgGame = [{
+    id:1,
+    img: "assets/item.png"
+},{
+    id:2,
+    img: "assets/item1.png"
+},{
+    id:3,
+    img: "assets/item2.png" 
+},{
+    id:4,
+    img: "assets/item3.png"
+},{
+    id:5,
+    img: "assets/item4.png"
+}]
+
+export const TextIn = [{
+    id:1,
+    h1: "290M+",
+    p: "Players Top Up"
+},{
+    id:2,
+    h1: "12.500",
+    p: "Games Available"
+},{
+    id:3,
+    h1: "99,9%",
+    p: "Happy Players"
+},{
+    id:4,
+    h1: "4.7",
+    p: "Rating Worldwide"
+}]
